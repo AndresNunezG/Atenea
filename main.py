83d1c6112fcab712ad31d9852d2099efdb4815f6
Camilo Núñez <camilonunez@Camilos-MacBook-Air.local>
2023-03-16 22:27:07 -0500

    styles

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 import openai
 import typer
 from dotenv import load_dotenv
-from rich import print
+from rich import box, print
 from rich.console import Console
+from rich.markdown import Markdown
+from rich.panel import Panel
 from rich.table import Table
 
 load_dotenv()
@@ -18,18 +20,13 @@ class Atenea(object):
 
     @classmethod
     def main(cls) -> None:
-        print("🤖 [bold green]Atenea[/bold green]")
-        table = Table("Command", "Description", header_style="green")
-        table.add_row("exit", "exit application")
-        table.add_row("new", "Create new chat")
-        print(table)
-
         console = Console()
+        cls.__print_intro()
         while True:
             content = cls.__parse_prompt()
             cls.__messages.append({"role": "user", "content": content})
 
-            with console.status("[bold green]"):
+            with console.status("[slate_blue3]", spinner="aesthetic"):
                 response = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo", messages=cls.__messages
                 )
@@ -37,7 +34,19 @@ class Atenea(object):
 
             cls.__messages.append({"role": "assistant", "content": response_content})
 
-            print(f"🤖: [white]{response_content}[/white]")
+            md = Markdown(response_content)
+            console.print(Panel(md, padding=1))
+
+    @classmethod
+    def __print_intro(cls) -> None:
+        print("✨ [bold slate_blue3]Camsky[/bold slate_blue3]", end=" ")
+        print("- [bold italic grey66]ChatGPT 3.5[/bold italic grey66]")
+        table = Table(box=box.ROUNDED)
+        table.add_column("Command", style="slate_blue3")
+        table.add_column("Description", style="white")
+        table.add_row("exit", "exit application")
+        table.add_row("new", "create new chat")
+        print(table)
 
     @classmethod
     def __parse_prompt(cls) -> str:
@@ -53,7 +62,7 @@ class Atenea(object):
 
     @classmethod
     def __ask_prompt(cls) -> str:
-        return typer.prompt("\nHow can i help you?")
+        return typer.prompt("\n▲ How can i help you?")
 
     @classmethod
     def __confirm_exit(cls) -> None:
@@ -64,7 +73,7 @@ class Atenea(object):
     @classmethod
     def __new_chat(cls) -> None:
         cls.__messages = [cls.__context]
-        print("🤖 New chat created")
+        print("💬 New chat created")
 
 
 if __name__ == "__main__":
